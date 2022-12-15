b196a7493056c93c6360203448f7f1cf00ece6f7
formegusto <formegusto@gmail.com>
2022-12-15 14:40:04 +0900

    ts : chain init

---
+++ ts-blockchain-exam/src/core/blockchain/chain.test.ts
@@ -0,0 +1,23 @@
+import { Chain } from "@core/blockchain/chain";
+
+describe("Chain 함수 테스트", () => {
+  let node: Chain = new Chain();
+
+  it("chain getter 테스트", () => {
+    console.log(node.chain);
+  });
+
+  it("length getter 테스트", () => {
+    console.log(node.length);
+  });
+
+  it("latestBlock getter 테스트", () => {
+    console.log(node.latestBlock);
+  });
+
+  it("addBlock() 함수 테스트", () => {
+    for (let i = 1; i <= 5; i++) node.addBlock([`Block #${i}`]);
+
+    console.log(node.chain);
+  });
+});
